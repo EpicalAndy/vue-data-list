5511d323709352eff02a6b7bc40a84e7debd6d53
Andy <lun9nin@inbox.ru>
2023-06-23 17:29:20 +0300

    Release

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <v-app>
-
+    <app-data-list-component></app-data-list-component>
   </v-app>
 </template>
 
 <script setup>
 
+import AppDataListComponent from "@/components/AppDataListComponent.vue";
 </script>
---
+++ src/components/AppButtonComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-btn>
+    <slot></slot>
+  </v-btn>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
